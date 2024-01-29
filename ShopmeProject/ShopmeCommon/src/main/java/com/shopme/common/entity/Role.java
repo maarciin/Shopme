24b772e89b8a7da7b7146489d63a1fb8cc79a6c9
MarcinG <gawlikmr@gmail.com>
2024-01-29 08:38:01 +0100

    Role entity, repository and tests

---
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -0,0 +1,33 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 150, nullable = false)
+    private String description;
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
