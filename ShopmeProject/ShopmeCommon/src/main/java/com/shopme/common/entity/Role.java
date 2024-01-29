2c541fb26f1d8b7218e4a7d7769e09daa937ef83
MarcinG <gawlikmr@gmail.com>
2024-01-29 12:43:56 +0100

    users.html + usersListing

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -50,8 +50,6 @@ public class Role {
 
     @Override
     public String toString() {
-        return "Role{" +
-                "name='" + name + '\'' +
-                '}';
+        return this.name;
     }
 }
