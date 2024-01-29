25f69b1a01bd7eaa45dc0e5f536a92ba5f165af3
MarcinG <gawlikmr@gmail.com>
2024-01-29 14:08:30 +0100

    user_form.html + adding new user

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,17 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
+    }
+    
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }
