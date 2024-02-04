483590bb95b878e8e6b53442a748d36f23f52d15
MarcinG <gawlikmr@gmail.com>
2024-02-04 10:50:50 +0100

    user login page

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.shopme.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,9 +17,20 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-
+    public SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+                )
+            .formLogin(form -> form
+                    .loginPage("/login")
+                    .usernameParameter("email")
+                    .permitAll()
+            );
         return http.build();
+    }
+
+    @Bean
+    WebSecurityCustomizer configureWebSecurity () {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
     }
 }
