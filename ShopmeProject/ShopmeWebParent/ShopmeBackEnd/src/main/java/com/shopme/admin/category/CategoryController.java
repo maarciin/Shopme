7e94e7665c9bd49c7c49f9ad94fe6ff44cd01d3f
MarcinG <gawlikmr@gmail.com>
2024-02-12 12:25:44 +0100

    categories pagination

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -1,6 +1,7 @@
 package com.shopme.admin.category;
 
 import com.shopme.admin.FileUploadUtil;
+import com.shopme.admin.user.UserService;
 import com.shopme.common.entity.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -20,17 +21,39 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/categories")
-    public String getCategories(@RequestParam(required = false) String sortDir, Model model) {
+    public String listFirstPage(@RequestParam(required = false) String sortDir, Model model) {
+        return listByPage(1, sortDir, model);
+    }
 
+    @GetMapping("/categories/page/{pageNum}")
+    public String listByPage(@PathVariable int pageNum, String sortDir, Model model) {
         if (sortDir == null || sortDir.isEmpty()) {
             sortDir = "asc";
         }
 
-        List<Category> listCategories = categoryService.listAll(sortDir);
+        CategoryPageInfo pageInfo = new CategoryPageInfo();
+
+        List<Category> listCategories = categoryService.listByPage(pageInfo, pageNum, sortDir);
         model.addAttribute("listCategories", listCategories);
 
         String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
         model.addAttribute("reverseSortDir", reverseSortDir);
+
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("totalItems", pageInfo.getTotalElements());
+        model.addAttribute("totalPages", pageInfo.getTotalPages());
+
+        long startCount = (long) (pageNum - 1) * CategoryService.ROOT_CATEGORIES_PER_PAGE + 1;
+        long endCount = startCount + CategoryService.ROOT_CATEGORIES_PER_PAGE - 1;
+
+        if (endCount > pageInfo.getTotalElements()) {
+            endCount = pageInfo.getTotalElements();
+        }
+
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("sortField", "name");
+        model.addAttribute("sortDir", sortDir);
 
         return "categories/categories";
     }
